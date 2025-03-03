799e3f102de3fa2f8d459d35cfa0575d6e145cd7
ritiksah141 <ritiksah141@gmail.com>
2025-03-03 02:00:09 +0000

    Backend src sorted

--- src/routes/serviceRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { getServices, addService } = require("../controllers/serviceController");
-const router = express.Router();
-
-router.get("/", getServices);
-router.post("/add", addService);
-
-module.exports = router;
